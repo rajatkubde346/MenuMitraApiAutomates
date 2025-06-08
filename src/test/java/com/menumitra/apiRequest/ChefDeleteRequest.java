090ce68188d903d578e15f37502931f3066ac223
rajatkubde346 <rajatkubde346@gmail.com>
2025-06-09 00:22:51 +0530

    New Changes 08-06-2025

---
+++ src/test/java/com/menumitra/apiRequest/ChefDeleteRequest.java
@@ -0,0 +1,40 @@
+package com.menumitra.apiRequest;
+
+public class ChefDeleteRequest {
+    private int update_user_id;
+    private int user_id;
+    private int outlet_id;
+    private String app_source;
+
+    public int getUpdate_user_id() {
+        return update_user_id;
+    }
+
+    public void setUpdate_user_id(int update_user_id) {
+        this.update_user_id = update_user_id;
+    }
+
+    public int getUser_id() {
+        return user_id;
+    }
+
+    public void setUser_id(int user_id) {
+        this.user_id = user_id;
+    }
+
+    public int getOutlet_id() {
+        return outlet_id;
+    }
+
+    public void setOutlet_id(int outlet_id) {
+        this.outlet_id = outlet_id;
+    }
+
+    public String getApp_source() {
+        return app_source;
+    }
+
+    public void setApp_source(String app_source) {
+        this.app_source = app_source;
+    }
+}
