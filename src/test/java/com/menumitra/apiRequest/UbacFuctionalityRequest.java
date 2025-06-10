5c16562349a4080dd90b070a59afd550195b1bc9
rajatkubde346 <rajatkubde346@gmail.com>
2025-06-10 15:40:55 +0530

    New Changes 10-06-2025

--- src/test/java/com/menumitra/apiRequest/UbacFuctionalityRequest.java
+++ src/test/java/com/menumitra/apiRequest/UbacFuctionalityRequest.java
@@ -3,6 +3,8 @@ package com.menumitra.apiRequest;
 public class UbacFuctionalityRequest {
     private int[] functionality_id;
     private int user_id;
+    private String functionality_name;
+    
 
     public int[] getFunctionality_id() {
         return functionality_id;
@@ -18,5 +20,13 @@ public class UbacFuctionalityRequest {
 
     public void setUser_id(int user_id) {
         this.user_id = user_id;
+    }
+
+    public String getFunctionality_name() {
+        return functionality_name;
+    }
+
+    public void setFunctionality_name(String functionality_name) {
+        this.functionality_name = functionality_name;
     }
 }
